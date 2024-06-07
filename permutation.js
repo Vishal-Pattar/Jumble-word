f7a585fb74929f76a90c1997ec9e9e419e2716cf
vishal-pattar <vmpnwc15002@gmail.com>
2024-06-07 22:16:26 +0530

    updated files

--- permutation.js
+++ permutation.js
@@ -1,164 +1,101 @@
+// Function to generate all combinations of a given character array
 function getCombinations(chars) {
-    var result = [];
-    var f = function (prefix, chars) {
-        for (var i = 0; i < chars.length; i++) {
+    const result = [];
+    const f = (prefix, chars) => {
+        for (let i = 0; i < chars.length; i++) {
             result.push(prefix + chars[i]);
             f(prefix + chars[i], chars.slice(i + 1));
         }
-    }
+    };
     f('', chars);
     return result;
 }
 
-function permute(word) { 
-    let arrperm = []
-    if (word.length < 2) {
-        arrperm.push(word);
-    }
+// Function to generate all permutations of a given word
+function permute(word) {
+    if (word.length < 2) return [word];
+
+    const arrperm = [];
     for (let i = 0; i < word.length; i++) {
-        let char = word[i]
-
-        let remainingChars = word.slice(0, i) + word.slice(i + 1, word.length)
-
-        for (let permutation of permute(remainingChars)) {
-            arrperm.push(char + permutation)
+        const char = word[i];
+        const remainingChars = word.slice(0, i) + word.slice(i + 1);
+        for (const permutation of permute(remainingChars)) {
+            arrperm.push(char + permutation);
         }
     }
-    return arrperm
+    return arrperm;
 }
 
-function generateperm(word) {
-    var ch = word.toUpperCase().split("");
-    var subring = getCombinations(ch);
-    var permar = [];
-    for (var x = 0; x < subring.length; x++) {
-        var par = permute(subring[x]);
-        for(var y = 0; y < par.length; y++){
-            permar.push(par[y]);
+// Function to generate all permutations of all combinations of a given word
+function generatePermutations(word) {
+    const chars = word.toUpperCase().split('');
+    const combinations = getCombinations(chars);
+    const permutations = new Set();
+
+    for (const combination of combinations) {
+        const perms = permute(combination);
+        for (const perm of perms) {
+            permutations.add(perm);
         }
     }
-    return permar;
+    return Array.from(permutations);
 }
 
-function chkdiction(word) {
-    perms = generateperm(word);
-    var fnllst = [];
-    for (var f = 0; f < perms.length; f++) {
-        var wd = perms[f];
-        var fit = Search(wd);
-        if (fit == 1) {
-            fnllst.push(wd);
+// Function to check if a word is in the dictionary
+function checkDictionary(word) {
+    const perms = generatePermutations(word);
+    const validWords = perms.filter(search);
+    return validWords;
+}
+
+// Function to check if a word starts with a given prefix
+function wordStarts(word, start) {
+    return word.startsWith(start);
+}
+
+// Function to check if a word ends with a given suffix
+function wordEnds(word, end) {
+    return word.endsWith(end);
+}
+
+// Function to check if a word meets length constraints
+function wordLength(word, length, minLength) {
+    return (length === 0 && minLength === 0) || word.length === length || word.length >= minLength;
+}
+
+// Function to check if a word contains given characters
+function wordContains(word, contains) {
+    if (!contains) return true;
+
+    for (let i = 0; i <= word.length - contains.length; i++) {
+        if (contains.split('').every((char, index) => word[i + index] === char || char === '_' || char === '?')) {
+            return true;
         }
     }
-    return fnllst;
+    return false;
 }
 
-function word_starts(word, start) {
-    var ln = start.length;
-    var ct = 0;
-    var res = false;
-    for (var x = 0; x < ln; x++) {
-        if (word[x] == start[x]) {
-            ct++;
-        }
-    }
-    if (ct == ln) {
-        res = true;
-    }
-    return res;
+// Function to apply constraints to the list of valid words
+function applyConstraints(word, length, minLength, start, end, contains) {
+    const validWords = checkDictionary(word);
+    return validWords.filter(w => 
+        wordStarts(w, start) && wordEnds(w, end) && wordLength(w, length, minLength) && wordContains(w, contains)
+    );
 }
 
-function word_ends(word, end) {
-    var lne = end.length;
-    var ln = word.length;
-    var ct = 0;
-    var res = false;
-    for (var x = ln - 1; x > ln - lne - 1; x--) {
-        if (word[x] == end[x - ln + lne]) {
-            ct++;
-        }
-    }
-    if (ct == lne) {
-        res = true;
-    }
-    return res;
-}
-
-function word_length(word, ln, minln) {
-    var res = false;
-    if (ln == 0 && minln == 0) {
-        res = true;
-    }
-    if (word.length == ln) {
-        res = true;
-    }
-    if (word.length >= minln && minln != 0) {
-        res = true;
-    }
-    return res;
-}
-
-function word_contains(word, cw) {
-    var ln = word.length;
-    var res = false;
-    if (cw.length != 0) {
-        for (var x = 0; x < ln; x++) {
-            if (word[x] == cw[0] || cw[0] == "_" || cw[0] == "?") {
-                var a = x;
-                var ct = 0;
-                var mn = Math.min(x + cw.length, ln);
-                for (var z = x; z < mn; z++) {
-                    if (word[z] == cw[z - a]) {
-                        ct++;
-                    }
-                    else if (cw[z - a] == "_" || cw[z - a] == "?") {
-                        ct++;
-                    }
-                }
-                if (ct == cw.length) {
-                    res = true;
-                }
-            }
-        }
-    }
-    else {
-        res = true;
-    }
-    return res;
-}
-
-
-function constrains(word, ln, minln, start, end, cw) {
-    var arr = chkdiction(word);
-    var res = [];
-    for (var w = 0; w < arr.length; w++) {
-        var cst = word_starts(arr[w], start);
-        var cen = word_ends(arr[w], end);
-        var cln = word_length(arr[w], ln, minln);
-        var cwc = word_contains(arr[w], cw);
-        if (cst == true && cen == true && cln == true && cwc == true) {
-            res.push(arr[w]);
-        }
-    }
-    var rset = new Set(res);
-    var result = Array.from(rset);
-    return result;
-}
-function show(){
-    var word = document.getElementById("word").value;
-    var ln = Number(document.getElementById("word_len").value);
-    var minln = Number(document.getElementById("word_mlen").value);
-    var start = document.getElementById("word_start").value;
-    var end = document.getElementById("word_end").value;
-    var cw = document.getElementById("word_cont").value;
-    var res = constrains(word,ln,minln,start,end,cw);
-    document.getElementById("two-letter").innerHTML = "";
-    if(res.length == 0){
-        document.getElementById("two-letter").innerHTML = '<div class="word_out">No word found</div>';
-    }
-    else{
-        for(var x=0; x < res.length; x++){
-            document.getElementById("two-letter").innerHTML += '<div class="word_out">' + res[x] + '</div>';
-        }
-    }
+// Function to display the results
+function show() {
+    const word = document.getElementById("word").value;
+    const length = Number(document.getElementById("word_len").value);
+    const minLength = Number(document.getElementById("word_mlen").value);
+    const start = document.getElementById("word_start").value;
+    const end = document.getElementById("word_end").value;
+    const contains = document.getElementById("word_cont").value;
+    
+    const results = applyConstraints(word, length, minLength, start, end, contains);
+    
+    const outputElement = document.getElementById("two-letter");
+    outputElement.innerHTML = results.length > 0 ? 
+        results.map(w => `<div class="word_out">${w}</div>`).join('') : 
+        '<div class="word_out">No word found</div>';
 }
